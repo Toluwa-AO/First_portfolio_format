3cd00689ec1389f77d63c8b2c4cf3405f96b7a19
Toluwa-AO <toluwa.ao@gmail.com>
2024-07-15 20:17:38 +0100

    im back

--- lib/widgets/herotext.dart
+++ lib/widgets/herotext.dart
@@ -283,7 +283,7 @@ class heroText extends StatelessWidget {
                 ),
               ),
             ),
-            SizedBox(
+            const SizedBox(
               height: 90,
             ),
             Center(
